6ade324c34e5e653f7f687de066e73fa476979b4
Schuyler Mortimer <smortimer@squareup.com>
2023-03-21 20:07:08 -0700

    the restart

---
+++ lua/smort/init.lua
@@ -0,0 +1,4 @@
+require("smort.remap")
+require("smort.lazy")
+require("smort.set")
+
---
+++ lua/smort/lazy.lua
@@ -0,0 +1,63 @@
+--- Install package manager
+--    https://github.com/folke/lazy.nvim
+--    `:help lazy.nvim.txt` for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
+    lazypath,
+  }
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	-- Fuzzy Finder (files, lsp, etc)
+	{ 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
+
+	{ -- Highlight, edit, and navigate code
+	'nvim-treesitter/nvim-treesitter',
+	dependencies = {
+		'nvim-treesitter/nvim-treesitter-textobjects',
+	},
+	config = function()
+		pcall(require('nvim-treesitter.install').update { with_sync = true })
+	end,
+	},
+
+	-- Theme
+	{
+		'folke/tokyonight.nvim',
+		config = function()
+			vim.cmd.colorscheme 'tokyonight-night'
+		end,
+	},
+
+	-- LET THERE BE LSP THAT WORKS
+	{
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		dependencies = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},         -- Required
+			{'hrsh7th/cmp-nvim-lsp'},     -- Required
+			{'hrsh7th/cmp-buffer'},       -- Optional
+			{'hrsh7th/cmp-path'},         -- Optional
+			{'saadparwaiz1/cmp_luasnip'}, -- Optional
+			{'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},             -- Required
+			{'rafamadriz/friendly-snippets'}, -- Optional
+		}
+	},
+
+}, {})
