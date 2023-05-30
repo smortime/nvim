657079199e7ef83de5abe97adbc710828e79a951
smortime <schuymortimer@gmail.com>
2023-05-29 17:02:33 -0700

    telescope stuff

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,2 +1,22 @@
 local builtin = require('telescope.builtin')
+
+-- Enable telescope fzf native, if installed
+pcall(require('telescope').load_extension, 'fzf')
+
 vim.keymap.set('n', '<Tab><Tab>', builtin.find_files, {})
+
+-- Open telescope if vim with no arguments
+vim.api.nvim_create_augroup('startup', { clear = true })
+vim.api.nvim_create_autocmd('VimEnter', {
+  group = 'startup',
+  pattern = '*',
+  callback = function()
+    -- Open file browser if argument is a folder
+    local arg = vim.api.nvim_eval('argv(0)')
+    if arg and (vim.fn.isdirectory(arg) ~= 0 or arg == "") then
+      vim.defer_fn(function()
+        builtin.find_files()
+      end, 10)
+    end
+  end
+})
--- lua/smort/remap.lua
+++ lua/smort/remap.lua
@@ -11,3 +11,10 @@ vim.keymap.set("n", "<leader>yy", "\"+Y")
 vim.keymap.set("n", "<leader>p", "\"+p")
 vim.keymap.set("v", "<leader>p", "\"+p")
 vim.keymap.set("n", "<leader>pp", "\"+P")
+
+-- Navigate windows
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
+
