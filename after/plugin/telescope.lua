e3c764552bd0be2a27098c1c5d967afd597e36ba
Schuyler Mortimer <smortimer@squareup.com>
2023-07-07 16:32:20 -0700

    telescope keybindings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,6 +4,8 @@ local builtin = require('telescope.builtin')
 pcall(require('telescope').load_extension, 'fzf')
 
 vim.keymap.set('n', '<Tab><Tab>', builtin.find_files, {})
+vim.keymap.set('n', '<Tab>g', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>sk', builtin.keymaps, {})
 
 -- Open telescope if vim with no arguments
 vim.api.nvim_create_augroup('startup', { clear = true })
