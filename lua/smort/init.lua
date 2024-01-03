ca89a68bed87cb330a39bb62259b3822f5722d07
Schuyler Mortimer <smortimer@nvidia.com>
2024-01-03 10:42:48 -0800

    default set spell

--- lua/smort/init.lua
+++ lua/smort/init.lua
@@ -5,11 +5,12 @@ require("smort.set")
 -- Run gofmt + goimport on save
 local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').goimport()
-  end,
-  group = format_sync_grp,
+    pattern = "*.go",
+    callback = function()
+        require('go.format').goimport()
+    end,
+    group = format_sync_grp,
 })
 
 vim.cmd("colorscheme nordfox")
+vim.cmd("set spell")
